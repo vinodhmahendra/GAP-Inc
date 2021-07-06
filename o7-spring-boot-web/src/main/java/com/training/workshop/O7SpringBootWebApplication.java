210474a3809c137a0692e794c901d1ffdc55ffff
Vinodh  Mahendra <vinodh.mahendra@gmail.com>
2021-07-06 08:02:43 +0000

    initial commit

---
+++ o7-spring-boot-web/src/main/java/com/training/workshop/O7SpringBootWebApplication.java
@@ -0,0 +1,23 @@
+package com.training.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class O7SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O7SpringBootWebApplication.class, args);
+	}
+
+//	@RequestMapping(path = "/",method = RequestMethod.GET)
+	@GetMapping("/")
+	public String greetings() {
+		return "Hello World!";
+	}
+}
