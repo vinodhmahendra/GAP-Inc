b86c445a48b6386099f0b5a3216e12d0a7cbdc3c
Vinodh  Mahendra <vinodh.mahendra@gmail.com>
2021-07-08 05:44:23 +0000

    commit message

---
+++ 14-customer-service-data-rest/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into customers (firstname,lastname,email) values ('vinodh','mahendra','vinodh.mahendra@gmail.com');
+insert into customers (firstname,lastname,email) values ('bhavya','vinodh','bhavya@collins.com');
+insert into customers (firstname,lastname,email) values ('samarth','vishnu','samarth.vishnu@gmail.com');
